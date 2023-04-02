2e438fdc9634259581ac6474176c0983e9bb5b91
MingMZ <1409247+MingMZ@users.noreply.github.com>
2023-04-02 11:05:53 +0800

    Add files via upload

---
+++ Dracula-Pro.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#ff9580", "#8aff80", "#ffff80", "#9580ff", "#ff80bf", "#80ffea", "#7970A9", "#454158", "#ff9580", "#8aff80", "#ffff80", "#9580ff", "#ff80bf", "#80ffea", "#ffffff"]);
+t.prefs_.set('foreground-color', "#f8f8f2");
+t.prefs_.set('background-color', "#22212c");
+t.prefs_.set('cursor-color', 'rgba(187,187,187,0.5)');
